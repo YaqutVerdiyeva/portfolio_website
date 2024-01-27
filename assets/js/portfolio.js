aad707a867582a212e9235355248058e32dfcdfd
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-01-27 17:33:07 +0400

    completed portfolio page

---
+++ assets/js/portfolio.js
@@ -0,0 +1,25 @@
+let BASE_URL = "http://localhost:3000/projects";
+let projects = document.querySelector(".projects");
+async function getData() {
+  let res = await axios.get(BASE_URL);
+  let data = await res.data;
+  data.map((el) => {
+    return (projects.innerHTML += `
+    <div class="project">
+    <div class="photo">
+      <img src="${el.photo}" alt="" />
+    </div>
+    <div class="text">
+      <p class="name">${el.text}</p>
+      <a href="#" class="learn-more">
+        <span
+          >Project <i class="fa-solid fa-arrow-right"></i
+        ></span>
+        <div class="liquid"></div>
+      </a>
+    </div>
+  </div>
+    `);
+  });
+}
+getData();
