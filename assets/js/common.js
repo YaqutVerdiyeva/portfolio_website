27523f6eddb89c51c193e494ba8ec4b4852e7728
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-01-28 00:04:24 +0400

    add scroll up btn and completed admin page for projects

--- assets/js/common.js
+++ assets/js/common.js
@@ -52,3 +52,24 @@ contactLink.addEventListener("mouseleave", () => {
 portfolioLink.addEventListener("mouseleave", () => {
   linkTextPortfolio.style.display = "none";
 });
+/////////////////////////////////////////////////////scrollup
+let scrollUpBtn = document.querySelector(".scrollup");
+scrollUpBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+window.addEventListener("scroll", function () {
+  scrollFunction();
+});
